9be652e6fa19c955648a8b2048c64a80e7ca5496
Eksity <103298458+Eksity@users.noreply.github.com>
2022-10-22 17:21:00 -0500

    Update 4_divisors.py

--- practicepython/4_divisors.py
+++ practicepython/4_divisors.py
@@ -1,1 +1,14 @@
 #Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
+def all_divisors(num):
+    divisor_list = []
+    for number in range(num + 1)[1:]:
+        if num % number == 0:
+            divisor_list.append(number)
+    return divisor_list
+
+def main():
+    print(all_divisors(int(input("Number: "))))
+
+if __name__ == "__main__":
+    main()
