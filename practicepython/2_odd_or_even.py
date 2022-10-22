2d4e9ed60b660219ea256a3423e9ebb017870354
Eksity <103298458+Eksity@users.noreply.github.com>
2022-10-21 23:40:10 -0500

    Solved problem 2

---
+++ practicepython/2_odd_or_even.py
@@ -0,0 +1,16 @@
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+#Extras:
+#1: If the number is a multiple of 4, print out a different message.
+#2: Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
+#If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+#main and extra #1
+def is_even(number):
+    if number % 4 == 0:
+        print("Divisible by 4!")
+    elif number % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd!")
+
+is_even(int(input()))
