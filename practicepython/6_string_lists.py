1b89952e77c1018d9e9fdbd80b53dabf188002d9
Eksity <103298458+Eksity@users.noreply.github.com>
2022-10-22 23:19:41 -0500

    solved problem 6

---
+++ practicepython/6_string_lists.py
@@ -0,0 +1,18 @@
+#Ask the user for a string and print out whether this string is a palindrome or not.
+
+def palindrome(str):
+    reverse = ""
+    for x in str:
+        reverse = f"{x}{reverse}"
+    if reverse == str:
+        return True
+    else:
+        return False
+
+def main():
+    palindrome(input("Input: "))
+
+if __name__ == "__main__":
+    main()
+
+    
